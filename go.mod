f6aa7480baa51acf6b7a7f0e669a20ff3ae695c9
johannesscr <johannes.scri@gmail.com>
2022-06-14 01:10:44 +0200

    feat/11 - added the Service type and test NewService function

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.17
 require (
 	github.com/dottics/dutil v0.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/johannesscr/micro v0.1.1 // indirect
 )
---
+++ service_test.go
@@ -0,0 +1,34 @@
+package bankserv
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewService(t *testing.T) {
+	// set the env vars NewService should automatically get them
+	schemeEnv := "BANK_SERVICE_SCHEME"
+	scheme := "https"
+	err := os.Setenv(schemeEnv, scheme)
+	if err != nil {
+		t.Errorf("unexpected error setting %s: %v", schemeEnv, err)
+	}
+	hostEnv := "BANK_SERVICE_HOST"
+	host := "bank.dottics.com"
+	err = os.Setenv(hostEnv, host)
+	if err != nil {
+		t.Errorf("unexpected error setting %s: %v", hostEnv, err)
+	}
+	token := "my-test-token"
+	ms := NewService(token)
+	if ms.URL.Scheme != scheme {
+		t.Errorf("expected Bank Service to have Scheme %s got %s",
+			scheme, ms.URL.Scheme,
+		)
+	}
+	if ms.URL.Host != host {
+		t.Errorf("expected Bank Service to have Host %s got %s",
+			host, ms.URL.Host,
+		)
+	}
+}
