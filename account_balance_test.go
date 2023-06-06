5200b21113229f6ac758c7bcf1ea77a9a5e0c851
Johannes Scribante <johannes.scribs@gmail.com>
2023-06-06 21:33:45 +0200

    task/181 - added the tests to create an account balance

--- account_balance_test.go
+++ account_balance_test.go
@@ -91,3 +91,91 @@ func TestService_GetAccountBalance(t *testing.T) {
 		})
 	}
 }
+
+func TestService_CreateAccountBalance(t *testing.T) {
+	tests := []struct {
+		name     string
+		abIn     AccountBalance
+		exchange *microtest.Exchange
+		abOut    AccountBalance
+		e        dutil.Error
+	}{
+		{
+			name: "permission required",
+			abIn: AccountBalance{
+				AccountUUID: uuid.MustParse("7ca43b18-4684-4c32-b894-c15395190d47"),
+				Date:        time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+				Balance:     0,
+			},
+			exchange: &microtest.Exchange{
+				Response: microtest.Response{
+					Status: http.StatusUnauthorized,
+					Body: `{
+						"message": "Unauthorised: Unable to process request",
+						"errors": {
+							"permission": ["permission required"]
+						}
+					}`,
+				},
+			},
+			abOut: AccountBalance{},
+			e: &dutil.Err{
+				Status: http.StatusUnauthorized,
+				Errors: dutil.Errors{
+					"permission": []string{"permission required"},
+				},
+			},
+		},
+		{
+			name: "successful",
+			abIn: AccountBalance{
+				AccountUUID: uuid.MustParse("bf87be5d-2245-4ae0-8882-c17bc5043346"),
+				Date:        time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+				Balance:     0,
+			},
+			exchange: &microtest.Exchange{
+				Response: microtest.Response{
+					Status: http.StatusCreated,
+					Body: `{
+						"message": "account balance created",
+						"data": {
+							"account_balance": {
+								"account_uuid": "bf87be5d-2245-4ae0-8882-c17bc5043346",
+								"date": "2020-05-11T00:05:00Z",
+								"balance": 1200,
+								"currency": "GBP"
+							}
+						},
+						"errors": {}
+					}`,
+				},
+			},
+			abOut: AccountBalance{
+				AccountUUID: uuid.MustParse("bf87be5d-2245-4ae0-8882-c17bc5043346"),
+				Date:        time.Date(2020, 5, 11, 0, 5, 0, 0, time.UTC),
+				Balance:     1200,
+			},
+			e: nil,
+		},
+	}
+
+	s := NewService("")
+	ms := microtest.MockServer(s.serv)
+	defer ms.Server.Close()
+
+	for i, tc := range tests {
+		name := fmt.Sprintf("%d: %s", i, tc.name)
+		t.Run(name, func(t *testing.T) {
+			ms.Append(tc.exchange)
+
+			ab, e := s.CreateAccountBalance(&tc.abIn, &http.Header{"x-dot-api-key": {"test"}})
+
+			if !dutil.ErrorEqual(e, tc.e) {
+				t.Errorf("got %v, want %v", e, tc.e)
+			}
+			if ab != tc.abOut {
+				t.Errorf("expected\n%v\n got\n%v", tc.abOut, ab)
+			}
+		})
+	}
+}
